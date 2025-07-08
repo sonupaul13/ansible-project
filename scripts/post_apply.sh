5865bc895a4f6fa34f6f86aaf6f14f4e71b97160
sonupaul13 <sonupaul213@gmail.com>
2025-07-08 15:02:56 +0530

    Added two resources

--- scripts/post_apply.sh
+++ scripts/post_apply.sh
@@ -12,7 +12,7 @@ terraform output -json vm_public_ips > ../vm_ips.json
 cd ../ansible
 
 rm -f inventory.txt
-echo "[mount_disk]" > inventory.txt
+echo "[web]" > inventory.txt
 
 mkdir -p ~/.ssh
 touch ~/.ssh/known_hosts
