f1ef4e7ed56b7a67e4abb63128e60e92d4e78d4a
penumarthigeeta <penumarthi.sree@gdn-commerce.com>
2025-07-08 16:06:58 +0530

    added

--- scripts/post_apply.sh
+++ scripts/post_apply.sh
@@ -81,4 +81,4 @@ echo "[Atlantis] Generated inventory:"
 cat inventory.txt
 
 echo "[Atlantis] Running Ansible playbook..."
-ansible-playbook -i inventory.txt site.yml
+ansible-playbook -i inventory.txt site.yml --tags install
