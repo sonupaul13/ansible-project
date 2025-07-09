3684dac89c849d0330f5f5c4cb2dc860b02ae902
penumarthigeeta <penumarthi.sree@gdn-commerce.com>
2025-07-09 13:02:13 +0530

    added

--- project_1/outputs.tf
+++ project_1/outputs.tf
@@ -1,6 +1,6 @@
 output "vm_public_ips" {
   value = [
-    for vm in module.vm_instance.vm_public_ips : {
+    for vm in values(module.vm_instance.vm_public_ips) : {
       ip       = vm.ip
       username = vm.username
     }
