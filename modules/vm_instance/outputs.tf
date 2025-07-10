b724e971e7f2861b9cfb1a4e98053e5d31566ca2
penumarthigeeta <penumarthi.sree@gdn-commerce.com>
2025-07-10 10:09:06 +0530

    added

--- modules/vm_instance/outputs.tf
+++ modules/vm_instance/outputs.tf
@@ -1,9 +1,10 @@
 output "vm_public_ips" {
   value = {
-    for name, vm in google_compute_instance.vm_sandeep_tf :
-    name => {
-      ip = vm.network_interface[0].access_config[0].nat_ip
-      username = vm.metadata["ssh-keys"] != null ? split(":", vm.metadata["ssh-keys"])[0] : null
+    for k, vm in google_compute_instance.vm_sandeep_tf :
+    k => {
+      ip       = vm.network_interface[0].access_config[0].nat_ip
+      username = vm.metadata["ssh-keys"] != null ? split(":", vm.metadata["ssh-keys"])[0] : "ansible-user"
+      tags     = var.instances[k].tags
     }
   }
 }
