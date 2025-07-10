361954a5cd26c3f7fef7388db45ce5b04aeeab7b
penumarthigeeta <penumarthi.sree@gdn-commerce.com>
2025-07-10 10:01:39 +0530

    added

--- project_1/outputs.tf
+++ project_1/outputs.tf
@@ -3,7 +3,10 @@ output "vm_public_ips" {
     for vm in values(module.vm_instance.vm_public_ips) : {
       ip       = vm.ip
       username = vm.username
-      tags = try(vm.tags, [])
+      tags     = [
+        for k, v in try(vm.tags, {}) :
+        k if v == "true" && contains(["mongo", "postgres", "solr"], k)
+      ]
     }
   ]
 }
